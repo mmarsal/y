ee378b1117ec2070dc515a41518848e1103f0f5b
Martin Marsal <mmarsal@stud.hs-heilbronn.de>
2023-10-23 15:10:08 +0200

    Restructure script for access patterns.

--- redis/app/program.js
+++ redis/app/program.js
@@ -1,16 +1,26 @@
 console.log("Running script.");
 
+let client = null;
+
 const createAndRunClient = async () => {
     try {
         console.log("Creating client.");
         const redis = require('redis');
-        let client = redis.createClient({
+        client = redis.createClient({
             url: 'redis://redis:6379',
         });
         client.on('error', err => console.log('Redis Client Error', err));
         await client.connect();
 
         console.log("Running access patterns.");
+        await postTweet();
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+};
+
+const postTweet = async () => {
+    try {
         await client.set('key', 'value');
         const value = await client.get('key');
         console.log("value", value)
