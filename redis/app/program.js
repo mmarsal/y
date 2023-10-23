a10d2793ed32ddb7a8d3fff1a5f288f64a1bc61f
Martin Marsal <mmarsal@stud.hs-heilbronn.de>
2023-10-23 14:48:29 +0200

    Fix nodejs script

--- redis/app/program.js
+++ redis/app/program.js
@@ -1,29 +1,22 @@
-console.log("TEST1");
+console.log("Running script.");
 
 const createAndRunClient = async () => {
     try {
-        console.log("TEST2")
-
+        console.log("Creating client.");
         const redis = require('redis');
         let client = redis.createClient({
-            legacyMode: true ,
             url: 'redis://redis:6379',
         });
         client.on('error', err => console.log('Redis Client Error', err));
         await client.connect();
 
-        // console.log("client", client)
-        console.log("TEST3")
-
+        console.log("Running access patterns.");
         await client.set('key', 'value');
         const value = await client.get('key');
-
         console.log("value", value)
     } catch (error) {
         console.error('An error occurred:', error);
     }
-
-    console.log("TEST4")
 };
 
 createAndRunClient();
